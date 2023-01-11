51c185d1952af233067bbcb26eaf4ee5a4fe4d15
psychobilli <richardsw1123@gmail.com>
2023-01-10 21:56:43 -0500

    Altered the calculation for spell damage because spell casters hit way too hard.  Used 63 because world bosses are level 63.  SD3 update.

--- src/game/Object/Unit.cpp
+++ src/game/Object/Unit.cpp
@@ -8771,7 +8771,7 @@ int32 Unit::CalculateSpellDamage(Unit const* target, SpellEntry const* spellProt
         spellProto->Effect[effect_index] != SPELL_EFFECT_KNOCK_BACK &&
         (spellProto->Effect[effect_index] != SPELL_EFFECT_APPLY_AURA || spellProto->EffectApplyAuraName[effect_index] != SPELL_AURA_MOD_DECREASE_SPEED))
         {
-            value = int32(value * 0.25f * exp(getLevel() * (70 - spellProto->spellLevel) / 1000.0f));
+            value = int32(value * 0.25f * exp(getLevel() * (63 - spellProto->spellLevel) / 1000.0f));
         }
 
     return value;
